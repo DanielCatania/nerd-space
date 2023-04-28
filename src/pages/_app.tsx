85c08d4c0873e0a5386a7a268b8097bfacd9f3e4
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-04-28 16:57:02 -0300

    adding styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import "@/styles/globals.css";
+import Globals from "@/styles/globals";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Globals />
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ src/styles/globals.ts
@@ -0,0 +1,76 @@
+import { createGlobalStyle } from "styled-components";
+
+const Globals = createGlobalStyle`
+  html,
+  body,
+  #__next {
+    height: 100%;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  html,
+  body,
+  div,
+  span,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  a,
+  abbr,
+  em,
+  img,
+  small,
+  strong,
+  u,
+  i,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  footer,
+  header,
+  nav,
+  section,
+  audio,
+  video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+`;
+
+export default Globals;
