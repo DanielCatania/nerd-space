e6bdb4355d911c2319774f6a6f78220ab24aebff
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-04-29 10:34:35 -0300

    adding patterns folder and calling header in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import type { AppProps } from "next/app";
+
 import { ThemeProvider } from "styled-components";
 import theme from "@/styles/theme";
 import "@/styles/reset.css";
 
+import Header from "@/components/patterns/Header/";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { Text } from "@/components/Text";
-import Header from "@/components/Header";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  return <></>;
 }
