20147017efdee800fd82c8284ae8ff31028d5fd8
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-04-29 01:12:57 -0300

    changing globals to reset globals in styled-components to a reset in css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import Globals from "@/styles/globals";
 import theme from "@/styles/theme";
+import "@/styles/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Globals />
       <Component {...pageProps} />
     </ThemeProvider>
   );
