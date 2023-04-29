d3903c51bd80daa74463f2f6e0612dd6a4d36103
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-04-29 10:22:45 -0300

    adding link in logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Link from "next/link";
 import Box from "./style";
 import { Text } from "../Text";
 
 export default function Header() {
   return (
     <Box>
-      <Text>NS | Nerd Space</Text>
+      <Link href="/">
+        <Text>NS | Nerd Space</Text>
+      </Link>
     </Box>
   );
 }
