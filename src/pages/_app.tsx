33acf5cc680ee363df9dd028efb586944db0f66e
Daniel Catania Rodrigues <dcataniarodrigues20@gmail.com>
2023-09-14 15:03:50 -0300

    fixing global style loading failure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,13 @@ import { AppProps } from "next/app";
 
 import { ThemeProvider } from "styled-components";
 import theme from "@/styles/theme";
-import GlobalStyle from "@/styles/global";
-
 import Header from "@/components/Header";
+
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
       <Header />
       <Component {...pageProps} />
     </ThemeProvider>
